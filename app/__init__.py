318a207a9c66c5c968a97ace5caabccf9bbb5147
Ian Odhiambo <you@example.com>
2018-09-01 08:50:49 +0300

    added bootstrap 4

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
 
 # get debuging from config.py
 # from .config import DevConfig
@@ -9,8 +13,18 @@ def create_news(config_name):
     # Initializing application object using the flask constructor
     app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
     # registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
 
     return app
