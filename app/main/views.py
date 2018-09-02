84b22e8ff859c26c2bbfea47ced3e2f3d2a32918
Ian Odhiambo <you@example.com>
2018-09-02 15:28:43 +0300

    import artcile model and request function to views

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from . import main  # importing data from main
-from ..requests import get_sources
-from ..models import Sources
+from ..requests import get_sources, get_articles
+from ..models import Sources, Articles
 
 
 @main.route('/')
@@ -16,3 +16,11 @@ def index():
     #     print(i.name)
     title = "hello world"
     return render_template('index.html', title=title, general=general, business=business, entertainment=entertainment, technology=technology, science=science)
+
+@main.route('/articles/<int:id>')
+def article(id):
+    '''
+    view all article based of each source
+    '''
+
+    article = 
